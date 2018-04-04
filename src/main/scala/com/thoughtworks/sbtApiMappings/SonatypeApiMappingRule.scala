6ac14b35c5e2de0e66d7fa3623d67cc606f85378
Yang Bo <atryyang@thoughtworks.com>
2018-04-04 11:01:47 +0800

    Support Scaladoc on sonatype.org for sbt plug-ins

--- src/main/scala/com/thoughtworks/sbtApiMappings/SonatypeApiMappingRule.scala
+++ src/main/scala/com/thoughtworks/sbtApiMappings/SonatypeApiMappingRule.scala
@@ -11,14 +11,23 @@ object SonatypeApiMappingRule extends AutoPlugin {
 
   override def trigger = allRequirements
 
-  private def moduleID: Attributed[File] => Option[(String, String, String)] = _.get(Keys.moduleID.key).map { moduleID =>
-    (moduleID.organization, moduleID.name, moduleID.revision)
+  private val JarBaseNameRegex = """(.*)\.jar""".r
+
+  private def baseNameAndModuleID: Attributed[File] => Option[(String, String, String, String)] = { jar =>
+    jar.data.getName match {
+      case JarBaseNameRegex(baseName) =>
+        jar.get(Keys.moduleID.key).map { moduleID =>
+          (baseName, moduleID.organization, moduleID.name, moduleID.revision)
+        }
+      case _ =>
+        None
+    }
   }
 
   private def sonatypeRule: PartialFunction[Attributed[File], URL] = {
-    case moduleID.extract(organization, libraryName, revision) =>
+    case baseNameAndModuleID.extract(baseName, organization, libraryName, revision) =>
       val organizationPath = organization.replace('.', '/')
-      url(s"https://oss.sonatype.org/service/local/repositories/public/archive/$organizationPath/$libraryName/$revision/$libraryName-$revision-javadoc.jar/!/index.html")
+      url(s"https://oss.sonatype.org/service/local/repositories/public/archive/$organizationPath/$libraryName/$revision/$baseName-javadoc.jar/!/index.html")
   }
 
   override def projectSettings = {
